c30850251b4ecfeb47f3e6ff5100ffbdab694e41
leonardo <leonardo.espinosa@everis.com>
2021-08-11 20:17:01 -0400

    front bank

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './interface/data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-banco';
+export class AppComponent implements OnInit {
+
+  public data: Array<Cliente> = [
+    {
+      id: '1',
+      rut: '26.247.043-1',
+      nombre: 'LEONARDO JOSE',
+      tipo_cuenta: 'CORRIENTE',
+      numerocuenta: '023181844484848455'
+    },
+    {
+      id: '2',
+      rut: '19.202.480-3',
+      nombre: 'INES VALENZUELA',
+      tipo_cuenta: 'AHORRO',
+      numerocuenta: '001284848484848444'
+    }
+  ];
+  ngOnInit(): void {
+    console.log('data: ', this.data);
+  }
+
 }
---
+++ src/app/interface/data.ts
@@ -0,0 +1,7 @@
+export interface Cliente {
+    id: string;
+    nombre: string;
+    rut: string;
+    tipo_cuenta: String;
+    numerocuenta: string;
+}
