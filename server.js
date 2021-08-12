054f2d58433e29b1aa74f6ace4b8d4cc401ebb5a
leonardo <leonardo.espinosa@everis.com>
2021-08-11 21:14:43 -0400

    modificacion server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/banco'));
+app.use(express.static('./dist/front-banco'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', { root: 'dist/front-banco' }),
